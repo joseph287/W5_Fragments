5fadc8310015b9d6d10505a40f3591c6e0efced4
joseph287 <joseph287@gmail.com>
2017-02-20 20:02:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mymac/w5_fragments/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mymac.w5_fragments;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void moveToTwo(View view){
+
+    startActivity(new Intent(MainActivity.this, Main2Activity.class));
+
+
+}
+}
+
